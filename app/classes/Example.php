07b111a22db2dee15177c4ba3dd0800676618736
rakibqww <rakib456@gmail.com>
2022-02-01 18:05:06 +0600

    Class11/2

---
+++ app/classes/Example.php
@@ -0,0 +1,79 @@
+<?php
+//
+//namespace App\classes;
+//
+//class Example{
+//    public $student = [];
+//
+//    public function index(){
+////        $this->student = [10, 20,'BITM', 10.20, true, false];
+////        //echo $this->student[0];
+////        foreach ($this->student as $key => $value){
+////            echo 'Index No - '. $key. ' '. 'value - '. $value.'<br/> ';
+////        }
+//
+//    $this->student = [
+//        0 => [
+//        'name' => 'Rakib Hasan', //=> - array Object
+//        'email' => 'rakib456@gmail.com', // ->php object
+//        'mobile' => [
+//            'mobile1' => '+880 1916-687307',
+//            'mobile2' => '+880 1916-687307'
+//        ]
+//            ],
+//        1 => [
+//            'name' => 'Rakib Hasan 2', //=> - array Object
+//            'email' => 'rakib456@gmail.com', // ->php object
+//            'mobile' => '+880 1916-687307'
+//            ],
+//        2 => [
+//            'name' => 'Rakib Hasan 3', //=> - array Object
+//            'email' => 'rakib456@gmail.com', // ->php object
+//            'mobile' => '+880 1916-687307'
+//        ],
+//        3 => 'BITM',
+//        4 => [
+//            'name' => 'Rakib Hasan 4', //=> - array Object
+//            'email' => 'rakib456@gmail.com', // ->php object
+//            'mobile' => [
+//                'mobile1' => '+880 1916-687307',
+//                'mobile2' => '+880 1916-687307'
+//            ]
+//        ],
+//        ];
+//
+//    echo '<pre>';
+//    //print_r($this->student);
+////    var_dump($this->student);
+//
+//        $_
+//
+//    //echo $this->student[1]['name'];
+//
+////        foreach ($this->student as $key => $item) {
+////            if ($key == 1) {
+////                echo $item['name'] . ' ' . $item['email'] . ' ' . $item['mobile'] . '<br/>';
+////                break;
+////            }
+////        }
+//
+//
+////        foreach ($this->student as $item) { //foreach ($this->student as $item) {
+////             //   echo $item['name'] . ' ' . $item['email'] . ' ' . $item['mobile'] . '<br/>';
+////            if (is_array($item)) {
+////                foreach ($item as $value) {
+////                    if (is_array($value)){
+////                        foreach ($value as $v_value){
+////                            echo $v_value. '<br/>';
+////                        }
+////                    }else{
+////                        echo $value . '<br/>';
+////                    }
+////                }
+////            }else{
+////                echo $item. '<br/>';
+////                }
+////            echo '<br/>';
+////        }
+//     }
+//}
